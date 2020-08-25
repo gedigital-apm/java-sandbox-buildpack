5b8cc0497c30a86b16a3fac9ab82ce149d8a94b4
GE Digital - APM <44354853+gedigital-apm@users.noreply.github.com>
2020-08-25 15:53:58 -0700

    Update app_dynamics_agent.rb

--- lib/java_buildpack/framework/app_dynamics_agent.rb
+++ lib/java_buildpack/framework/app_dynamics_agent.rb
@@ -85,8 +85,6 @@ module JavaBuildpack
 
       def node_name(java_opts, credentials)
         name = credentials['node-name'] || @configuration['default_node_name']
-        appd_name = name.to_s
-        print " Downloading viewproxy #{appd_name.blue.bold} "
         java_opts.add_system_property('appdynamics.agent.nodeName', name.to_s)
       end
 
